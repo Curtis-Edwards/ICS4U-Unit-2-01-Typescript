d3b8c446d917bada84121486e7e27b702812df5e
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-20 09:36:53 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,18 +7,18 @@
  */
 import { CurtisStack } from './CurtisStack'
 
-let fruits = new CurtiStack()
+let fruits = new CurtisStack()
 fruits.push("apple")
 fruits.push("banana")
 console.log("fruits: " + fruits.show())
 
-let colours = new CurtiStack()
+let colours = new CurtisStack()
 colours.push("red")
 colours.push("green")
 colours.push("blue")
 console.log("colours: " + colours.show())
 
-let shapes = new CurtiStack()
+let shapes = new CurtisStack()
 animals.push("square")
 animals.push("triangle")
 animals.push("circle")
